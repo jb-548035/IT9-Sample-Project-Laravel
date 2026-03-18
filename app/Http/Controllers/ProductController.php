8e9da5a3ed911119ff2e6e07f44296d11eec9cf3
jb-548035 <j.barrios.548035@umindanao.edu.ph>
2026-03-18 09:03:14 +0800

    Inserted code in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+    $products = Product::all();
+
+    return view('products.index', [
+        'items' => $products
+    ]);
+    }
+    public function store(Request $request) 
+    {
+        Product::create([
+            'name' => $request->name123,
+            'price' => $request->price123
+        ]);
+        return redirect('/products');
+    }
 }
